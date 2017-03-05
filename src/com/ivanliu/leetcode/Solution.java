d89e187c21d0ae1928ad5f774fe8ce1dab15834c
IvanLiu1218 <ivanliu1218@hotmail.com>
2017-03-05 12:56:57 +0800

    LEC513: accepted

--- src/com/ivanliu/leetcode/Solution.java
+++ src/com/ivanliu/leetcode/Solution.java
@@ -6600,4 +6600,49 @@ public class Solution {
         }
         return result.toArray(new String[]{});
     }
+    
+    /**
+     *  [Easy]
+     *  #513. Find Bottom Left Tree Value
+     *  
+     *  Given a binary tree, find the leftmost value in the last row of the tree.
+     *  
+     *  Example 1:
+     *  Input:
+     *  
+     *      2
+     *     / \
+     *    1   3
+     *  
+     *  Output:
+     *  1
+     *  
+     *  Example 2: 
+     *  Input:
+     *  
+     *          1
+     *         / \
+     *        2   3
+     *       /   / \
+     *      4   5   6
+     *         /
+     *        7
+     *  
+     *  Output:
+     *  7
+     *  
+     *  Note: You may assume the tree (i.e., the given root node) is not NULL.
+     */
+    public int findBottomLeftValue(TreeNode root) {
+    	Deque<TreeNode> queue = new ArrayDeque<TreeNode>();
+    	queue.offerLast(root);
+    	if (root.right != null) queue.offerLast(root.right);
+    	if (root.left != null) queue.offerLast(root.left);
+    	while (queue.size() > 1) {
+    		TreeNode node = queue.pollFirst();
+    		if (node.right != null) queue.offerLast(node.right);
+    		if (node.left != null) queue.offerLast(node.left);
+    	}
+        return queue.pollFirst().val;
+    }
 }
