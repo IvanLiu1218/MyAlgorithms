5a9e2bcafeb1f48add01fa3f4a31ced24c2015d9
Mr Ivan <ivanliu1218@hotmail.com>
2018-04-10 21:42:52 +0800

    238: add problem description

--- src/com/ivanliu/leetcode/Solution.java
+++ src/com/ivanliu/leetcode/Solution.java
@@ -4584,8 +4584,25 @@ public class Solution {
         node.val = node.next.val;
         node.next = node.next.next;
     }
-    
-    /**
+
+	/**
+	 *  [Medium]
+	 *  #238. Product of Array Except Self
+	 *  Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to
+	 *  the product of all the elements of nums except nums[i].
+	 *  Solve it without division and in O(n).
+	 *
+	 *  For example, given [1,2,3,4], return [24,12,8,6].
+	 *  Follow up:
+	 *  Could you solve it with constant space complexity? (Note: The output array does not count as extra space
+	 *  for the purpose of space complexity analysis.)
+	 */
+	public int[] productExceptSelf(int[] nums) {
+		int[] result = new int[nums.length];
+		return result;
+	}
+
+	/**
      *  [Easy]
      *  #242. Valid Anagram
      *  
