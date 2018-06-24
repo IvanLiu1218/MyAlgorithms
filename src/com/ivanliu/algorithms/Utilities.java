36b1bcb3f2c63b40e829af3057bd5b213b70232f
Mr Ivan <ivanliu1218@hotmail.com>
2018-06-24 11:25:11 +0800

    parmutation and partition

--- src/com/ivanliu/algorithms/Utilities.java
+++ src/com/ivanliu/algorithms/Utilities.java
@@ -1,26 +1,216 @@
 package com.ivanliu.algorithms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Utilities {
 
-    public static int[] randomPermutation(int size) {
-        int[] array = new int[size];
-        for (int i = 0; i < size; ++i) {
+    // get random number between [x, y)
+    public static int random(int x, int y) {
+        return new Random().nextInt(y - x) + x;
+    }
+
+    private static void swap(int[] array, int x, int y) {
+        int temp = array[x];
+        array[x] = array[y];
+        array[y] = temp;
+    }
+
+    /**
+     *  -------------------------------------------
+     *  Permutation
+     *  -------------------------------------------
+     */
+    public static int[] randomPermutation(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; ++i) {
             array[i] = i;
         }
-        Random r = new Random();
-        for (int i = 0; i < size - 1; ++i) {
+        for (int i = 0; i < n - 1; ++i) {
             int x = i;
-            int y = r.nextInt(size - i) + i;  // random[i, n)
+            int y = random(i, n);  // random[i, n)
             swap(array, x, y);
         }
         return array;
     }
 
-    private static void swap(int[] a, int x, int y) {
-        int temp = a[x];
-        a[x] = a[y];
-        a[y] = temp;
+    public static void reverse(int[] array, int begin, int end) {
+        int i = begin;
+        int j = end - 1;
+        while (i < j) {
+            swap(array, i++, j--);
+        }
+    }
+
+    public static boolean nextPermutation(int[] array, int begin, int end) {
+        if (begin == end || begin + 1 == end) return false;
+        for (int i = end - 2; begin <= i; --i) {
+            int j = i + 1;
+            if (array[i] >= array[j]) continue;
+            for (int k = end - 1; j <= k; --k) {
+                if (array[i] >= array[k]) continue;
+                swap(array, i, k);
+                reverse(array, j, end);
+                return true;
+            }
+        }
+        reverse(array, begin, end);
+        return false;
+    }
+
+    public static boolean prevPermutation(int[] array, int begin, int end) {
+        if (begin == end || begin + 1 == end) return false;
+        for (int i = end - 2; begin <= i; --i) {
+            int j = i + 1;
+            if (array[i] <= array[j]) continue;
+            for (int k = end - 1; j <= k; --k) {
+                if (array[i] <= array[k]) continue;
+                swap(array, i, k);
+                reverse(array, j, end);
+                return true;
+            }
+        }
+        reverse(array, begin, end);
+        return false;
+    }
+
+    public static List<int[]> allPermutation(int[] array) {
+        Arrays.sort(array);
+        List<int[]> all = new ArrayList<>();
+        do {
+            int[] copy = new int[array.length];
+            System.arraycopy(array, 0, copy, 0, array.length);
+            all.add(copy);
+        } while (Utilities.nextPermutation(array, 0, array.length));
+        return all;
+    }
+
+    /**
+     *  -------------------------------------------
+     *  Subset
+     *  -------------------------------------------
+     */
+
+    /**
+     *  -------------------------------------------
+     *  Partition
+     *  -------------------------------------------
+     */
+    public static int integerPartition(int n, int k) {
+        if (n < 1 || k < 1) return -1; // ERROR
+        if (n == 1 || k == 1) return 1;
+        if (n < k) return integerPartition(n, n);
+        if (n == k) return 1 + integerPartition(n, k - 1);
+        // if n > k
+        return integerPartition(n - k, k) + integerPartition(n, k - 1);
+    }
+
+    private static List<List<Integer>> allIntegerPartitions;
+    public static List<List<Integer>> integerPartition(int n) {
+        allIntegerPartitions = new ArrayList<>();
+        integerPartition(n, n, new ArrayList<>());
+        return allIntegerPartitions;
+    }
+
+    private static int integerPartition(int n, int k, List<Integer> list) {
+        if (n < 1 || k < 1) return -1; // ERROR
+        if (n == 1) {
+            List<Integer> copy = new ArrayList<>(list);
+            copy.add(1);
+            allIntegerPartitions.add(copy);
+            return 1;
+        } else if (k == 1) {
+            List<Integer> copy = new ArrayList<>(list);
+            for (int i = 0; i < n; ++i) {
+                copy.add(1);
+            }
+            allIntegerPartitions.add(copy);
+            return 1;
+        } else if (n < k) {
+            return integerPartition(n, n, list);
+        } else if (n == k) {
+            List<Integer> copy = new ArrayList<>(list);
+            copy.add(n);
+            allIntegerPartitions.add(copy);
+            return 1 + integerPartition(n, k - 1, list);
+        } else {  // n < k
+            list.add(k);
+            int num1 = integerPartition(n - k, k, list);
+            list.remove(new Integer(k));
+            int num2 = integerPartition(n, k - 1, list);
+            return num1 + num2;
+        }
+    }
+
+    public static int setPartition(int n) {
+        int num = 0;
+        for (int i = 1; i <= n; ++i) {
+            num += setPartition(n, i);
+        }
+        return num;
+    }
+
+    public static int setPartition(int n, int k) {
+        if (n < 1 || k == 0) return -1; // ERROR
+        if (k == 1 || n == k) return 1;
+        return setPartition(n - 1, k - 1) + k * setPartition(n - 1, k);
+    }
+
+    private static List<List<List<Integer>>> allSetPartitons;
+    public static List<List<List<Integer>>> setPartition(int[] nums, int k) {
+        allSetPartitons = new ArrayList<>();
+        setPartition(nums, nums.length, k, new ArrayList<>());
+        return allSetPartitons;
+    }
+    public static int setPartition(int[] nums, int len, int k, List<Integer> list) {
+        if (nums == null || len < 1 || k == 0 ) return -1; // ERROR
+        if (k == 1) {
+            List<List<Integer>> copy = new ArrayList<>();
+            copy.add(list);
+            List<Integer> l = new ArrayList<>();
+            for (int i = 0; i < len; ++i) {
+                l.add(nums[i]);
+            }
+            copy.add(l);
+            allSetPartitons.add(copy);
+            return 1;
+        } else if (len == k) {
+            List<List<Integer>> ll = new ArrayList<>();
+            for (int i = 0; i < len; ++i) {
+                List<Integer> l = new ArrayList<>();
+                l.add(nums[i]);
+                ll.add(l);
+            }
+            for (int x = 0; x < k; ++x) {
+                List<List<Integer>> copy = new ArrayList<>();
+                for (int i = 0; i < ll.size(); ++i) {
+                    List<Integer> l = new ArrayList<>();
+                    if (i == x) {
+                        l.addAll(list);
+                    }
+                    for (int j = 0; j < ll.get(i).size(); ++j) {
+                        l.add(ll.get(i).get(j));
+                    }
+                    copy.add(l);
+                }
+                allSetPartitons.add(copy);
+            }
+            return 1;
+        } else {
+            list.add(nums[len - 1]);
+            int num1 = setPartition(nums, len - 1, k - 1, list);
+            int num2 = k * setPartition(nums, len - 1, k, list);
+            return num1 + num2;
+        }
+    }
+
+    public static List<List<List<Integer>>> setPartition(int[] nums) {
+        allSetPartitons = new ArrayList<>();
+        for (int i = 1; i <= nums.length; ++i) {
+            setPartition(nums, nums.length, i, new ArrayList<>());
+        }
+        return allSetPartitons;
     }
 }
--- src/com/ivanliu/algorithms/UtilitiesTest.java
+++ src/com/ivanliu/algorithms/UtilitiesTest.java
@@ -1,8 +1,12 @@
 package com.ivanliu.algorithms;
 
+import com.intellij.ide.browsers.JavaScriptDebuggerStarter;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class UtilitiesTest {
 
@@ -21,4 +25,111 @@ public class UtilitiesTest {
         System.out.println(Arrays.toString(array));
     }
 
+    @Test
+    public void testReverse() {
+        int[] nums = new int[4];
+        for (int i = 0; i < nums.length; ++i) {
+            nums[i] = i;
+        }
+        System.out.println(Arrays.toString(nums));
+        Utilities.reverse(nums, 0, nums.length);
+        System.out.println(Arrays.toString(nums));
+
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        Utilities.reverse(nums, 1, nums.length);
+        System.out.println(Arrays.toString(nums));
+    }
+    @Test
+    public void testNextPermutation() {
+        int size = 4;
+        int[] nums = new int[size];
+        for (int i = 0; i < nums.length; ++i) {
+            nums[i] = i;
+        }
+        do {
+            System.out.println(Arrays.toString(nums));
+        } while (Utilities.nextPermutation(nums, 0, nums.length));
+    }
+
+    @Test
+    public void testAllPermutation() {
+        int[] nums = new int[] {1,2,0,3};
+        List<int[]> all = Utilities.allPermutation(nums);
+        System.out.println(all.size());
+        for (int[] array : all) {
+            System.out.println(Arrays.toString(array));
+        }
+    }
+
+    @Test
+    public void testPrevPermutation() {
+        int[] nums = new int[] {3,2,1,0};
+        do {
+            System.out.println(Arrays.toString(nums));
+        } while (Utilities.prevPermutation(nums, 0, nums.length));
+    }
+
+    @Test
+    public void testIntegerPartition1() {
+        assertEquals(627, Utilities.integerPartition(20, 20));
+    }
+
+    @Test
+    public void testIntegerPartition2() {
+        List<List<Integer>> all = Utilities.integerPartition(1);
+        all.stream().forEach(list -> System.out.println(Arrays.toString(list.toArray())));
+
+        all = Utilities.integerPartition(2);
+        all.stream().forEach(list -> System.out.println(Arrays.toString(list.toArray())));
+
+        all = Utilities.integerPartition(3);
+        all.stream().forEach(list -> System.out.println(Arrays.toString(list.toArray())));
+
+        all = Utilities.integerPartition(4);
+        all.stream().forEach(list -> System.out.println(Arrays.toString(list.toArray())));
+
+        all = Utilities.integerPartition(5);
+        all.stream().forEach(list -> System.out.println(Arrays.toString(list.toArray())));
+
+        all = Utilities.integerPartition(6);
+        all.stream().forEach(list -> System.out.println(Arrays.toString(list.toArray())));
+    }
+
+    @Test
+    public void testSetPartition1() {
+        assertEquals( 1, Utilities.setPartition(4, 1));
+        assertEquals( 7, Utilities.setPartition(4, 2));
+        assertEquals( 6, Utilities.setPartition(4, 3));
+        assertEquals( 1, Utilities.setPartition(4, 4));
+    }
+
+    @Test
+    public void testSetPartition2() {
+        assertEquals(15, Utilities.setPartition(4));
+    }
+
+    @Test
+    public void testSetPartition3() {
+        int[] nums = new int[] {1,2,3,4};
+        List<List<List<Integer>>> all = Utilities.setPartition(nums, 2);
+        for (int i = 0; i < all.size(); ++i) {
+            System.out.println(Arrays.toString(all.get(i).toArray()));
+        }
+
+//        nums = new int[] {1,2,3,4};
+//        all = Utilities.setPartition(nums, 3);
+//        for (int i = 0; i < all.size(); ++i) {
+//            System.out.println(Arrays.toString(all.get(i).toArray()));
+//        }
+    }
+
+    @Test
+    public void testSetPartition4() {
+        int[] nums = new int[] {1,2};
+        List<List<List<Integer>>> all = Utilities.setPartition(nums);
+        for (int i = 0; i < all.size(); ++i) {
+            System.out.println(Arrays.toString(all.get(i).toArray()));
+        }
+    }
 }
